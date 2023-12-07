37ff12e0ba35c182f7478e59bb8eea30337badde
poulet <65782158+pouleetbasquaise@users.noreply.github.com>
2023-12-07 09:22:20 +0100

    AAAAH

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function click(newColor) {
     var colour = document.getElementById("change");
     colour.style.color = newColor;
 }
+let sequence = [];
+let humanSequence = [];
+const startButton = document.querySelector('.js-start');
+
+//function start(){
+    //let colourGame = ["vert","rouge","rouge","bleu"];
+    // for (let i = 0; i < 4; i++) {
+    //     colourGame[i] = 3*Math.random();//0 = Jaune =,1 vert 2 bleu 3 rouge
+    // 
+    //colourGame[0]
+
+    
+//}
+function startGame() {
+    startButton.classlist.add('hidden');
+}
+
+startButton.addEventListener('click', startGame);
